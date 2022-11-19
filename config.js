97ade05be4668fbf7813a335632de4457c924ae9
unknown <boba.obda@gmail.com>
2022-11-19 20:07:28 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    app: {
+        px: '+',
+        token: 'TOKEN DE TON BOT',
+        owners: 'ID BUYER', //id buyer
+        funny: '1035322112350621808', //id Développeur
+        color: '2f3136', //couleur par défaut
+        footer: 'Yoko Perso by Funny',
+        maxVol: '150',
+        everyoneMention: false,
+        hostedBy: true,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+}
