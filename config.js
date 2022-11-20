9609149d064c9db7dd859adba1a19cdbad8e7840
dokbayee <117679052+dokbayee@users.noreply.github.com>
2022-11-20 12:32:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     app: {
         px: '+',
-        token: 'TOKEN DE TON BOT',
-        owners: 'ID BUYER', //id buyer
+        token: 'MTA0MTEzNDIzNzIyODczMjQ1Nw.GWW3gF.srJnIRJYsecCaghSGKo1P-oMEdMHBDIVAgMseQ',
+        owners: '1010470226430808134', //id buyer
         funny: '1035322112350621808', //id Développeur
         color: '2f3136', //couleur par défaut
         footer: 'Yoko Perso by Funny',
